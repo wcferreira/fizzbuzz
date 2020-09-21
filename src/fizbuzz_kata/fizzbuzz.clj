4ad61f01afa821316490a8aa22a5d03205e8d6e2
Wagner Ferreira <wcarvalho.ferreira@gmail.com>
2020-09-21 17:21:34 -0300

    refactor: creates function to check if a given value is multiple of a given number

--- src/fizbuzz_kata/fizzbuzz.clj
+++ src/fizbuzz_kata/fizzbuzz.clj
@@ -1,10 +1,14 @@
 (ns fizbuzz-kata.fizzbuzz)
 
+(defn is-multiple?
+  [value div]
+  (= 0 (mod value div)))
+
 (defn fizzbuzz
   [value]
   (cond
-    (= 0 (mod value 3)) "Fizz"
-    (= 0 (mod value 5)) "Buzz"
+    (is-multiple? value 3) "Fizz"
+    (is-multiple? value 5) "Buzz"
     :else (str value)))
 
 
